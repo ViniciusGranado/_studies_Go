19913178c8cd5b6234e5b5e260b043ba17211363
Vinicius Granado <vinicius.r.granado@gmail.com>
2022-04-11 21:11:04 -0300

    Update Movies component

--- web-project/frontend/src/pages/Movies/Movies.tsx
+++ web-project/frontend/src/pages/Movies/Movies.tsx
@@ -1,5 +1,26 @@
+import { useEffect, useState } from 'react';
+import { Movie } from '../../models/models';
+
 export const Movies = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    setMovies([
+      { id: 1, title: 'The Shawshank Redemption', runtime: 142 },
+      { id: 1, title: 'The Godfather', runtime: 175 },
+      { id: 1, title: 'The Dark Knight', runtime: 153 },
+    ]);
+  }, []);
+
   return (
-    <div>Movies</div>
+    <>
+      <h2>Chose a movie</h2>
+
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
+      </ul>
+    </>
   );
-}
+};
