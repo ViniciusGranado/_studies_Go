2729025ed031e9d5fff41a7b5c8011badc1b23cc
Vinicius Granado <vinicius.r.granado@gmail.com>
2022-04-13 17:13:37 -0300

    Added Movie fetch

--- web-project/frontend/src/models/models.ts
+++ web-project/frontend/src/models/models.ts
@@ -1,5 +1,13 @@
 export interface MovieItem {
-  id: number;
-  title: string;
-  runtime: number;
+	id: number;
+	title: string;
+	description: string;
+	year: number;
+	release_date: string;
+	runtime: number;
+	rating: number;
+	mpaa_rating: string;
+	genres: {
+    [id: number]: string;
+  };
 }
--- web-project/frontend/src/pages/Movie/Movie.tsx
+++ web-project/frontend/src/pages/Movie/Movie.tsx
@@ -4,22 +4,70 @@ import { MovieItem } from '../../models/models';
 
 export const Movie = () => {
   const [movie, setMovie] = useState<MovieItem>();
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
 
   const { id } = useParams();
 
   useEffect(() => {
     if (id) {
-      setMovie({
-        id: parseInt(id),
-        title: 'Some movie',
-        runtime: 150,
-      });
+      fetch(`http://localhost:4000/v1/movie/${id}`)
+        .then((response) => {
+          if (response.status !== 200) {
+            const err = new Error();
+            err.message = `Invalid response code: ${response.status}`;
+
+            setError(err);
+          }
+
+          return response.json();
+        })
+        .then((json) => {
+          setMovie(json.movie);
+          setIsLoaded(true);
+          setError(null);
+        });
     }
   }, [id]);
 
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (!isLoaded) {
+    return <p>Loading...</p>;
+  }
+
+  if (!movie) {
+    return <p>No movies on the database</p>;
+  }
+
+  let genres: string[];
+  if (movie.genres) {
+    genres = Object.values(movie.genres);
+  } else {
+    genres = [];
+  }
+
   return (
     <>
-      <h2>Movie: {movie?.title}</h2>
+      <h2>
+        Movie: {movie.title} ({movie.year})
+      </h2>
+
+      <div className='float-start'>
+        <small>Rating: {movie.mpaa_rating}</small>
+      </div>
+      <div className='float-end'>
+        {genres.map((m, index) => (
+          <span className='badge bg-secondary me-1' key={index}>
+            {m}
+          </span>
+        ))}
+      </div>
+      <div className='clearfix' />
+
+      <hr />
 
       <table className='table table-compact table-striped'>
         <thead></thead>
@@ -30,8 +78,13 @@ export const Movie = () => {
               <strong>Title:</strong>
             </td>
             <td>
-              <strong>{movie?.title}</strong>
+              <strong>{movie.title}</strong>
             </td>
+          </tr>
+
+          <tr>
+            <td><strong>Description:</strong></td>
+            <td>{movie.description}</td>
           </tr>
 
           <tr>
@@ -39,7 +92,7 @@ export const Movie = () => {
               <strong>Run time:</strong>
             </td>
             <td>
-              <strong>{movie?.runtime}</strong>
+              <strong>{movie.runtime}</strong>
             </td>
           </tr>
         </tbody>
