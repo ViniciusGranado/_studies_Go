2781d335efee15cf1802d007e4d2fe00e6b409e9
Vinicius Granado <vinicius.r.granado@gmail.com>
2022-04-13 15:28:26 -0300

    Added fetching on movies

--- web-project/frontend/src/pages/Movies/Movies.tsx
+++ web-project/frontend/src/pages/Movies/Movies.tsx
@@ -4,26 +4,34 @@ import { Link } from 'react-router-dom';
 
 export const Movies = () => {
   const [movies, setMovies] = useState<MovieItem[]>([]);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
-    setMovies([
-      { id: 1, title: 'The Shawshank Redemption', runtime: 142 },
-      { id: 2, title: 'The Godfather', runtime: 175 },
-      { id: 3, title: 'The Dark Knight', runtime: 153 },
-    ]);
+    fetch('http://localhost:4000/v1/movies')
+      .then((response) => response.json())
+      .then((json) => {
+        setMovies(json.movies);
+        setIsLoaded(true);
+      });
   }, []);
 
   return (
     <>
-      <h2>Chose a movie</h2>
+      {!isLoaded ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <h2>Chose a movie</h2>
 
-      <ul>
-        {movies.map((movie) => (
-          <li key={movie.id}>
-            <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
-          </li>
-        ))}
-      </ul>
+          <ul>
+            {movies.map((movie) => (
+              <li key={movie.id}>
+                <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </>
   );
 };
