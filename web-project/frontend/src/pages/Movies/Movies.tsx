67ba859da6b6babc4c32ae18f82947b27fc69e8b
Vinicius Granado <vinicius.r.granado@gmail.com>
2022-04-13 15:42:40 -0300

    Added error handling

--- web-project/frontend/src/pages/Movies/Movies.tsx
+++ web-project/frontend/src/pages/Movies/Movies.tsx
@@ -5,33 +5,46 @@ import { Link } from 'react-router-dom';
 export const Movies = () => {
   const [movies, setMovies] = useState<MovieItem[]>([]);
   const [isLoaded, setIsLoaded] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
     fetch('http://localhost:4000/v1/movies')
-      .then((response) => response.json())
+      .then((response) => {
+        if (response.status !== 200) {
+          const err = new Error();
+          err.message = `Invalid response code: ${response.status}`;
+
+          setError(err);
+        }
+
+        return response.json();
+      })
       .then((json) => {
         setMovies(json.movies);
         setIsLoaded(true);
+        setError(null);
       });
   }, []);
 
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (!isLoaded) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <>
-      {!isLoaded ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          <h2>Chose a movie</h2>
+      <h2>Chose a movie</h2>
 
-          <ul>
-            {movies.map((movie) => (
-              <li key={movie.id}>
-                <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
-              </li>
-            ))}
-          </ul>
-        </>
-      )}
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
