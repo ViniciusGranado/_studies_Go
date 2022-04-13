ddb37312c8238e0ea2c2507c65364009338f6cb2
Vinicius Granado <vinicius.r.granado@gmail.com>
2022-04-12 21:09:12 -0300

    Added getAllMovies handler

--- web-project/backend/cmd/api/movie-handlers.go
+++ web-project/backend/cmd/api/movie-handlers.go
@@ -25,8 +25,25 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 	movie, err := app.models.DB.Get(id)
 
 	err = app.writeJson(w, http.StatusOK, movie, "movie")
+
+	if err != nil {
+		app.errorJson(w, err)
+		return
+	}
 }
 
 func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
+	movies, err := app.models.DB.All()
 
+	if err != nil {
+		app.errorJson(w, err)
+		return
+	}
+
+	err = app.writeJson(w, http.StatusOK, movies, "movies")
+
+	if err != nil {
+		app.errorJson(w, err)
+		return
+	}
 }
--- web-project/backend/models/movies-db.go
+++ web-project/backend/models/movies-db.go
@@ -69,10 +69,78 @@ func (m *DBModel) Get(id int) (*Movie, error) {
 
 	movie.MovieGenre = genres
 
-
 	return &movie, nil
 }
 
-func (m *DBModel) All(id int) ([]*Movie, error) {
-	return nil, nil
+func (m *DBModel) All() ([]*Movie, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+
+	defer cancel()
+
+	query := `SELECT * FROM movies ORDER BY title`
+
+	rows, err := m.DB.QueryContext(ctx, query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var movies []*Movie
+
+	for rows.Next() {
+		var movie Movie
+
+		err := rows.Scan(
+			&movie.ID,
+			&movie.Title,
+			&movie.Description,
+			&movie.Year,
+			&movie.ReleaseDate,
+			&movie.Runtime,
+			&movie.Rating,
+			&movie.MPAARating,
+			&movie.CreatetaAt,
+			&movie.UpdatedAt,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		genreQuery := `SELECT mg.id, mg.movie_id, mg.genre_id, g.genre_name
+		FROM movies_genres mg
+		JOIN genres g
+		ON g.id = mg.genre_id
+		WHERE mg.movie_id = $1`
+
+		genreRows, _ := m.DB.QueryContext(ctx, genreQuery, movie.ID)
+
+		genres := make(map[int]string)
+
+		for genreRows.Next() {
+			var mg MovieGenre
+
+			err := genreRows.Scan(
+				&mg.ID,
+				&mg.MovieId,
+				&mg.GenreId,
+				&mg.Genre.GenreName,
+			)
+
+			if err != nil {
+				return nil, err
+			}
+
+			genres[mg.ID] = mg.Genre.GenreName
+		}
+
+		genreRows.Close()
+
+		movie.MovieGenre = genres
+		movies = append(movies, &movie)
+	}
+
+	return movies, nil
 }
